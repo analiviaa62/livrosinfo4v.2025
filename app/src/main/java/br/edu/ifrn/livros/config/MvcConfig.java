ea22b45830d82d5a897a8d43d940c5b0bae279c9
MadsonXP <madsonnatan85@gmail.com>
2025-12-18 22:03:41 -0300

    Criação do arquivo Mvc

---
+++ app/src/main/java/br/edu/ifrn/livros/config/MvcConfig.java
@@ -0,0 +1,28 @@
+package br.edu.ifrn.livros.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Cria a pasta "uploads" na raiz do projeto se não existir
+        exposeDirectory("uploads", registry);
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+
+        registry.addResourceHandler("/" + dirName + "/**")
+                .addResourceLocations("file:/" + uploadPath + "/");
+    }
+}
