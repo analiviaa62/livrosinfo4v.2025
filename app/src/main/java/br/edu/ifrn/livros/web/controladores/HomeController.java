83bd867b084ae7e96fdcae6738f69a2a29c4c36f
MadsonXP <madsonnatan85@gmail.com>
2025-12-19 17:03:37 -0300

    Finalizando Projeto v1

--- app/src/main/java/br/edu/ifrn/livros/web/controladores/HomeController.java
+++ app/src/main/java/br/edu/ifrn/livros/web/controladores/HomeController.java
@@ -2,6 +2,7 @@ package br.edu.ifrn.livros.web.controladores;
 
 import br.edu.ifrn.livros.persistencia.repositorio.LivroRepository;
 import br.edu.ifrn.livros.persistencia.repositorio.UsuarioRepository;
+import br.edu.ifrn.livros.persistencia.repositorio.EmprestimoRepository; // 1. Importar repositório
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,12 +17,18 @@ public class HomeController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private EmprestimoRepository emprestimoRepository; // 2. Injetar repositório
+
     @GetMapping("/")
     public String index(Model model) {
         // Busca os totais para mostrar nos cards
         model.addAttribute("totalLivros", livroRepository.count());
         model.addAttribute("totalUsuarios", usuarioRepository.count());
         
-        return "index"; // Vai abrir o arquivo index.html
+        // 3. Adicionar o total de empréstimos ao modelo
+        model.addAttribute("totalEmprestimos", emprestimoRepository.count());
+        
+        return "index";
     }
 }
